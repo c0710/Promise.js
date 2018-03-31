153eee6280172ca88ca7b5f45faead58cc6493fb
Evan <c0710@foxmail.com>
2018-03-31 22:02:01 +0800

    加入状态机制

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ var log = console.log
 function P (fn) {
   // 储存成功时的回调
   var doneList = []
-  // 实例的方法，用来注册异步事件
-  this.then = function (done, fail) {
-    doneList.push(done)
-    return this
+  var state = 'pending'
+  /**
+   * then() 方法返回一个  Promise 它最多需要有两个参数：Promise的成功和失败情况的回调函数。
+   *
+   * @param onResolved 已完成回调函数
+   * @param onRejected 已失败回调函数
+   * @return {Promise}
+   */
+  this.then = function (onResolved, onRejected) {
+    switch (state) {
+      case 'pending':
+        log('pending')
+        doneList.push(onResolved)
+        return this
+      case 'fulfilled':
+        log('fulfilled')
+        onResolved()
+        return this
+    }
   }
   /**
    *  如果fn(promise接受的函数)是同步函数的话，resolve就会在then之前执行，此时doneList还是空的
    *  所以用setTimeout使其成为异步函数
    * */
   function resolve () {
+    state = 'fulfilled'
     setTimeout(function () {
       doneList.forEach(function (fulfill) {
         fulfill()
@@ -22,10 +38,10 @@ function P (fn) {
 }
 
 var p = new P(function(resolve){
-  // setTimeout(resolve, 1000);
-  log('Start')
-  resolve()
+  setTimeout(resolve, 1000);
+  // resolve()
 });
 p
-  .then(function(){console.log('success')})
-  .then(function () { console.log('success2') })
+  .then(function(){console.log('111')})
+  .then(function () { console.log('2222') })
+
