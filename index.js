f18a7155a4eeb55bf5923660f3c352095f8160d0
Evan <c0710@foxmail.com>
2018-04-01 13:18:26 +0800

    链式调用

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var log = console.log
+
 function P (fn) {
   // 储存成功时的回调
   var doneList = []
   var state = 'pending'
+  var value = null
   /**
    * then() 方法返回一个  Promise 它最多需要有两个参数：Promise的成功和失败情况的回调函数。
    *
@@ -13,35 +15,74 @@ function P (fn) {
   this.then = function (onResolved, onRejected) {
     switch (state) {
       case 'pending':
-        log('pending')
         doneList.push(onResolved)
         return this
       case 'fulfilled':
-        log('fulfilled')
         onResolved()
         return this
     }
   }
+
+  function handle (cb) {
+    if (state === 'pending') {
+      doneList.push(cb)
+      return
+    }
+    // 若then中没有传递任何东西
+    if (!cb.onFulfilled) {
+      cb.resolve(value)
+      return
+    }
+    //
+    var ret = cb.onFulfilled(value)
+    cb.resolve(ret)
+  }
+
   /**
    *  如果fn(promise接受的函数)是同步函数的话，resolve就会在then之前执行，此时doneList还是空的
    *  所以用setTimeout使其成为异步函数
    * */
-  function resolve () {
+  function resolve (newValue) {
     state = 'fulfilled'
+    var value = newValue
     setTimeout(function () {
       doneList.forEach(function (fulfill) {
-        fulfill()
+        var tmp = fulfill(value)
+        if (value instanceof Promise) {
+          var newP = tmp
+          for (i++; i < doneList.length; i++) {
+            newP.then(doneList[i]);
+          }
+        } else {
+          value = tmp
+        }
       })
     }, 0)
   }
+
   fn(resolve)
 }
 
-var p = new P(function(resolve){
-  setTimeout(resolve, 1000);
+var p = new P(function (resolve) {
+  setTimeout(function () {
+    resolve('from p')
+  }, 1000);
   // resolve()
 });
+var p2 = function () {
+  return  new P(function (resolve) {
+    setTimeout(function (args) {
+      resolve('inner Promise')
+    }, 2000)
+  })
+}
 p
-  .then(function(){console.log('111')})
-  .then(function () { console.log('2222') })
+  .then(function (data) {
+    console.log('111', data)
+    return 'aaa'
+  })
+  .then(p2)
+  .then(function (data) {
+    console.log('2222', data)
+  })
 
