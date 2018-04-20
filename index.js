63059830134d1c74f396403b7a32abc7d768eab4
Evannn <c0710@foxmail.com>
2018-04-20 11:38:29 +0800

    链式操作 初步功能完成

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function isArray (arr) {
 }
 
 function P (executor) {
-    log('construct P')
     if (!isFunction(executor)) {
         throw new TypeError('fn must be a function!')
     }
@@ -24,7 +23,6 @@ function P (executor) {
     self.value = null              // Promise当前的值
 
     function resolve (val) {
-        console.log('执行resolve')
         if (self.status === 'pending') {
             self.status = 'fulfilled'
             self.value = val
@@ -58,6 +56,7 @@ P.prototype.then = function (onResolved, onRejected) {
     var self = this
     var promise2
     // 如果then的参数不是函数，默认直接resolve出接受到的value值或抛出错误
+    // (传入非函数的onResolved会发生值穿透)
     onResolved = isFunction(onResolved) ? onResolved : function (v) { return v}
     onRejected = isFunction(onRejected) ? onRejected : function (e) { throw e}
 
@@ -69,11 +68,12 @@ P.prototype.then = function (onResolved, onRejected) {
             try{
                 var x = onResolved(self.value)
                 // 如果onResolved的返回值是一个Promise对象，直接取它的结果做为promise2的结果
-                if(x instanceof P) {
-                    x.then(resolve, reject)
-                }
-                // 否则，以onResolved的返回值作为结果resolve出去
-                resolve(x)
+                // if(x instanceof P) {
+                //     x.then(resolve, reject)
+                // }
+                // // 否则，以onResolved的返回值作为结果resolve出去
+                // resolve(x)
+                resolvePromise(promise2, x, resolve, reject)
             }catch (e) {
                 reject(e)
             }
@@ -84,9 +84,7 @@ P.prototype.then = function (onResolved, onRejected) {
         return promise2 = new P(function(resolve, reject) {
             try{
                 var x = onRejected(self.value)
-                if(x instanceof P) {
-                    x.then(resolve, reject)
-                }
+                resolvePromise(promise2, x, resolve, reject)
             }catch (e) {
                 reject(e)
             }
@@ -98,9 +96,7 @@ P.prototype.then = function (onResolved, onRejected) {
             self.onResolvedCallback.push(function (value) {
                 try{
                     var x = onResolved(self.value)
-                    if (x instanceof P) {
-                        x.then(resolve, reject)
-                    }
+                    resolvePromise(promise2, x, resolve, reject)
                 } catch (e) {
                     reject(e)
                 }
@@ -108,9 +104,7 @@ P.prototype.then = function (onResolved, onRejected) {
             self.onRejectedCallback.push(function (value) {
                 try{
                     var x = onRejected(self.value)
-                    if (x instanceof P) {
-                        x.then(resolve, reject)
-                    }
+                    resolvePromise(promise2, x, resolve, reject)
                 } catch (e) {
                     reject(e)
                 }
@@ -123,4 +117,57 @@ P.prototype.then = function (onResolved, onRejected) {
 P.prototype.catch = function (onRejected) {
     return this.then(null, onRejected)
 }
+
+/**
+ * resolvePromise函数即为根据x的值来决定promise2的状态的函数
+ * 也即标准中的[Promise Resolution Procedure](https://promisesaplus.com/#point-47)
+ * x为`promise2 = promise1.then(onResolved, onRejected)`里`onResolved/onRejected`的返回值
+ * `resolve`和`reject`实际上是`promise2`的`executor`的两个实参，因为很难挂在其它的地方，所以一并传进来。
+ * 相信各位一定可以对照标准把标准转换成代码，这里就只标出代码在标准中对应的位置，只在必要的地方做一些解释
+ * */
+function resolvePromise(promise2, x, resolve, reject) {
+    var then
+    var thenCalledOrThrow = false
+
+    if (promise2 === x) {
+        return reject(new TypeError('Chaining cycle detected for promise!'))
+    }
+
+    if (x instanceof Promise) {
+        if (x.status === 'pending') { //because x could resolved by a Promise Object
+            x.then(function(v) {
+                resolvePromise(promise2, v, resolve, reject)
+            }, reject)
+        } else { //but if it is resolved, it will never resolved by a Promise Object but a static value;
+            x.then(resolve, reject)
+        }
+        return
+    }
+
+    if ((x !== null) && ((typeof x === 'object') || (typeof x === 'function'))) {
+        try {
+            then = x.then //because x.then could be a getter
+            if (typeof then === 'function') {
+                then.call(x, function rs(y) {
+                    if (thenCalledOrThrow) return
+                    thenCalledOrThrow = true
+                    return resolvePromise(promise2, y, resolve, reject)
+                }, function rj(r) {
+                    if (thenCalledOrThrow) return
+                    thenCalledOrThrow = true
+                    return reject(r)
+                })
+            } else {
+                resolve(x)
+            }
+        } catch (e) {
+            if (thenCalledOrThrow) return
+            thenCalledOrThrow = true
+            return reject(e)
+        }
+    } else {
+        resolve(x)
+    }
+}
+
 
