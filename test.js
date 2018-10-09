79fda28e962d5aac96ab835380347f00b94b652b
Evannn <c0710@foxmail.com>
2018-10-09 23:08:28 +0800

    P.resolve P.reject 以及catch的实现

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 var P = require('./lib/index');
 
 
+/*
 function getId() {
-    return new P(function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
         setTimeout(function () {
             console.log('get id.......');
-            resolve('id')
+            reject('id')
         }, 600)
     })
 }
 
 function getNameById(id) {
-    return new P(function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
         setTimeout(function () {
             console.log('get name')
             resolve('wang' + id)
@@ -25,8 +26,60 @@ function sayName(name) {
 function sayErr(err) {
     console.log('err: ' + err)
 }
-getId()
+P.reject('cat').then(getId)
     .then(getNameById)
-    .then(sayName, function (err) {
-        console.log(err)
-    })
+    .then(sayName).catch(function (err) {
+    console.log('catch err ' + err)
+})
+*/
+
+function async1() {
+    return new Promise(
+        (resolve, reject) => {
+            console.log('async1 start');
+            setTimeout(() => {
+                resolve('async1 finished')
+            }, 1000);
+        }
+    );
+}
+
+function async2() {
+    return new Promise(
+        (resolve, reject) => {
+            console.log('async2 start');
+            setTimeout(() => {
+                resolve('async2 finished')
+            }, 1000);
+        }
+    );
+}
+
+function async3() {
+    return new Promise(
+        (resolve, reject) => {
+            console.log('async3 start');
+            setTimeout(() => {
+                resolve('async3 finished');
+            }, 1000);
+        }
+    );
+}
+
+async1()
+    .then(
+        data => {
+            console.log(data);
+            return async2();
+        })
+    .then(
+        data => {
+            console.log(data);
+            return async3();
+        }
+    )
+    .then(
+        data => {
+            console.log(data);
+        }
+    );
